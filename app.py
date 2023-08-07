25a145afa4a6aedb5173cb6ccf6ed2a372f8db5a
rahulputhukkot <rahulputhukkot@gmail.com>
2023-08-07 15:35:23 +0100

    QTabWidget

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ import PySide6.QtCore
 from PySide6.QtGui import QPalette, QColor
 from PySide6.QtWidgets import (
     QApplication,
-    QHBoxLayout,
     QMainWindow,
     QPushButton,
-    QStackedLayout,
-    QVBoxLayout,
+    QTabWidget,
     QWidget
     )
 
@@ -28,42 +26,14 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("My App")
 
-        pagelayout = QVBoxLayout()
-        button_layout = QHBoxLayout()
-        self.stacklayout = QStackedLayout()
+        tabs = QTabWidget()
+        tabs.setTabPosition(QTabWidget.West)
+        tabs.setMovable(True)
 
-        pagelayout.addLayout(button_layout)
-        pagelayout.addLayout(self.stacklayout)
-
-        btn = QPushButton("red")
-        btn.pressed.connect(self.activate_tab_1)
-        button_layout.addWidget(btn)
-        self.stacklayout.addWidget(Color("red"))
-
-        btn = QPushButton("green")
-        btn.pressed.connect(self.activate_tab_2)
-        button_layout.addWidget(btn)
-        self.stacklayout.addWidget(Color("green"))
-
-        btn = QPushButton("yellow")
-        btn.pressed.connect(self.activate_tab_3)
-        button_layout.addWidget(btn)
-        self.stacklayout.addWidget(Color("yellow"))
-
-        widget = QWidget()
-        widget.setLayout(pagelayout)
-        self.setCentralWidget(widget)
-
-    def activate_tab_1(self):
-        self.stacklayout.setCurrentIndex(0)
-
-    def activate_tab_2(self):
-        self.stacklayout.setCurrentIndex(1)
-
-    def activate_tab_3(self):
-        self.stacklayout.setCurrentIndex(2)
-
-
+        for color in (["red", "green", "blue", "yellow"]):
+            tabs.addTab(Color(color), color)
+        
+        self.setCentralWidget(tabs)
 
 app = QApplication(sys.argv)
 
