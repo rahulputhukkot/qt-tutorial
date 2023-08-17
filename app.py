ee47b2a7b91fb34f83151acc615f1d3f301ecd34
rahulputhukkot <rahulputhukkot@gmail.com>
2023-08-18 00:45:05 +0100

    QDialog

--- app.py
+++ app.py
@@ -1,67 +1,64 @@
 """
-REF: https://www.pythonguis.com/tutorials/pyside6-actions-toolbars-menus/
+REF: https://www.pythonguis.com/tutorials/pyside6-dialogs/
 """
 
 import sys
+
 from PySide6.QtWidgets import (
-    QMainWindow, QApplication, QCheckBox,
-    QLabel, QToolBar, QStatusBar
+  QApplication,
+  QDialog,
+  QDialogButtonBox,
+  QLabel,
+  QMainWindow,
+  QPushButton,
+  QVBoxLayout
 )
-from PySide6.QtGui import QAction, QIcon, QKeySequence
-from PySide6.QtCore import Qt, QSize
+
+class CustomDialog(QDialog):
+  # Passing a parent object so that this window appears near the parent object in this case the mainwindow 
+  def __init__(self, parent=None):
+    super().__init__(parent)
+
+    self.setWindowTitle("Hello this is a custom modal")
+
+    QBtn = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
+
+    self.buttonBox = QDialogButtonBox(QBtn)
+    self.buttonBox.accepted.connect(self.accept)
+    self.buttonBox.rejected.connect(self.reject)
+
+    self.layout = QVBoxLayout()
+    message = QLabel("Something happened is that OK?")
+    self.layout.addWidget(message)
+    self.layout.addWidget(self.buttonBox)
+    self.setLayout(self.layout)
+
+
 
 class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
-  def __init__(self):
-    super(MainWindow, self).__init__()
+        self.setWindowTitle("My App")
 
-    self.setWindowTitle("My Awesome App")
+        button = QPushButton("Press me for a dialog!")
+        button.clicked.connect(self.button_clicked)
+        self.setCentralWidget(button)
 
-    label = QLabel("Hello")
-    label.setAlignment(Qt.AlignCenter)
+    def button_clicked(self, s):
+        print("click", s)
 
-    self.setCentralWidget(label)
+        # pass the parent as the parameter to the custom dialog to make it appear near the parent
+        dlg = CustomDialog(self) 
+        if  dlg.exec():
+           print('Success!!')
+        else :
+           print("Cancel!")
 
-    toolbar = QToolBar("My main toolbar")
-    toolbar.setIconSize(QSize(16, 16))
-    self.addToolBar(toolbar)
-
-    #NOTE: QAction needs a QObject passed as a parameter to act as the parent for the action
-    # in this case we pass self to have the QMainWindow as its parent
-    button_action = QAction(QIcon("icons/acorn.png"),"&First Button", self)
-    button_action.setStatusTip( "This is your first button")
-    button_action.triggered.connect(self.onMyToolBarButtonCLick)
-    button_action.setCheckable(True) # This makes the button in the toolbar togglable
-    button_action.setShortcut(QKeySequence("Ctrl+f"))
-    toolbar.addAction(button_action)
-
-    toolbar.addSeparator()
-
-    button_action_2 = QAction(QIcon("icons/android.png"), "&Second Button", self)
-    button_action_2.setStatusTip("This is the second button")
-    button_action_2.triggered.connect(self.onMyToolBarButtonCLick)
-    button_action_2.setCheckable(True)
-    button_action_2.setShortcut(QKeySequence("Ctrl+s"))
-    toolbar.addAction(button_action_2)
-
-    toolbar.addWidget(QLabel("Hello"))
-    toolbar.addWidget(QCheckBox())
-
-    self.setStatusBar(QStatusBar(self))
-    
-    menu = self.menuBar()
-
-    file_menu = menu.addMenu("&File") # The & helps to select the menu using Alt + key that follows
-    file_menu.addAction(button_action)
-    file_menu.addSeparator()
-
-    file_submenu = file_menu.addMenu("Submenu")
-    file_submenu.addAction(button_action_2)
-
-  def onMyToolBarButtonCLick(self, s):
-    print('onMyToolbarButtonClick',s)
 
 app = QApplication(sys.argv)
-w = MainWindow()
-w.show()
+
+window = MainWindow()
+window.show()
+
 app.exec()
